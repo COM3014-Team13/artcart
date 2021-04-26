82e08d165c388d2d1008508d8a2c3d9246d6a0e9
Kris <christiandobrev1@gmail.com>
2021-04-26 02:11:11 +0100

    MongoDB, mongoose setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { loginRouter } from './routes/login';
@@ -29,6 +30,22 @@ app.get('/api/user', (req, res) => {
     res.send('Login/Register!');
   });
 
+const start = async() => {
+  try {
+    await mongoose.connect('mongodb://user-mongo-srv:27017/user',{
+       useNewUrlParser: true,
+       useUnifiedTopology: true, 
+       useCreateIndex: true 
+    });
+    console.log('Connected to MongoDB');
+  } catch (err){
+    console.log(err);
+}
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!!');
 });
+
+};
+
+start();
+
