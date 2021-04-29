78b584f169c173fa7184c9dc4958a7aca9731642
Kris <christiandobrev1@gmail.com>
2021-04-29 03:16:35 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-interface Color {
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-    red: number;
-    blue: number;
-}
 
-const color: Color = {
-    red:20,
-    blue:15
-};
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
 
-console.log(color);
 
-export default color; 
+
+
+
