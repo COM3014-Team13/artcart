7a8ffcee5a871ec2a554817b011a5e5fb4c230a6
Dave Williams <davejjwilliams@gmail.com>
2021-05-28 20:43:21 +0200

    updates

--- common/src/middlewares/current-user.ts
+++ common/src/middlewares/current-user.ts
@@ -2,10 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface UserPayload {
-  id: string;
-  name: string;
-  email: string;
-  
+  obj: object;
 }
 
 declare global {
